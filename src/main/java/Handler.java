44a7f9638b5b0cf3438c24a743524df33ff895b2
glebov <constantine.glebov@gmail.com>
2023-05-25 11:40:34 +0700

    new branch changes from Home work 2 - Forms

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,8 +1,9 @@
 
 import java.io.BufferedOutputStream;
+import java.io.IOException;
 
 @FunctionalInterface
 public interface Handler {
 
-    void handle(Request request, BufferedOutputStream responseStream);
+    void handle(Request request, BufferedOutputStream responseStream) throws IOException;
 }
